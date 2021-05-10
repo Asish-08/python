96e446420bc2bc73e9c8ce027b9761a399dcbe46
Addanki Asish <54019861+Asish-08@users.noreply.github.com>
2021-05-10 15:50:21 +0530

    Update listexample.py

--- listexample.py
+++ listexample.py
@@ -2,9 +2,9 @@ txt = 'but soft what light in yonder window breaks'
 words = txt.split()
 t = list()
 for word in words:
-t.append((len(word), word))
+  t.append((len(word), word))
 t.sort(reverse=True)
 res = list()
 for length, word in t:
-res.append(word)
+  res.append(word)
 print(res)
